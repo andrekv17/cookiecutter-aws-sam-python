402c763157d1177b17c14c5b0fb543c2b09ad8d0
kirdyasheva <kirdyasheva98@gmail.com>
2018-11-05 06:38:46 +0300

    Tests for getting messages and running on AWS lambda were added

--- {{ cookiecutter.project_name }}/tests/unit/test_handler.py
+++ {{ cookiecutter.project_name }}/tests/unit/test_handler.py
@@ -1,8 +1,10 @@
 import json
 import pytest
 from first_function import app
+import os
 
-{% if cookiecutter.include_apigw == "y" %}
+{ % if cookiecutter.include_apigw == "y" %}
+
 @pytest.fixture()
 def apigw_event():
     """ Generates API GW Event"""
@@ -37,41 +39,41 @@ def apigw_event():
         },
         "headers": {
             "Via":
-            "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+                "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
             "Accept-Language":
-            "en-US,en;q=0.8",
+                "en-US,en;q=0.8",
             "CloudFront-Is-Desktop-Viewer":
-            "true",
+                "true",
             "CloudFront-Is-SmartTV-Viewer":
-            "false",
+                "false",
             "CloudFront-Is-Mobile-Viewer":
-            "false",
+                "false",
             "X-Forwarded-For":
-            "127.0.0.1, 127.0.0.2",
+                "127.0.0.1, 127.0.0.2",
             "CloudFront-Viewer-Country":
-            "US",
+                "US",
             "Accept":
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Upgrade-Insecure-Requests":
-            "1",
+                "1",
             "X-Forwarded-Port":
-            "443",
+                "443",
             "Host":
-            "1234567890.execute-api.us-east-1.amazonaws.com",
+                "1234567890.execute-api.us-east-1.amazonaws.com",
             "X-Forwarded-Proto":
-            "https",
+                "https",
             "X-Amz-Cf-Id":
-            "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+                "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
             "CloudFront-Is-Tablet-Viewer":
-            "false",
+                "false",
             "Cache-Control":
-            "max-age=0",
+                "max-age=0",
             "User-Agent":
-            "Custom User Agent String",
+                "Custom User Agent String",
             "CloudFront-Forwarded-Proto":
-            "https",
+                "https",
             "Accept-Encoding":
-            "gzip, deflate, sdch"
+                "gzip, deflate, sdch"
         },
         "pathParameters": {
             "proxy": "/examplepath"
@@ -83,20 +85,100 @@ def apigw_event():
         "path": "/examplepath"
     }
 
+
 def test_lambda_handler(apigw_event):
     ret = app.lambda_handler(apigw_event, "")
     assert ret['statusCode'] == 200
     assert ret['body'] == json.dumps({'hello': 'world'})
 
-{% else %}
+
+{ % else %}
 
 @pytest.fixture()
 def lambda_event():
     """ Generates Lambda Event"""
 
-    return { "foo": "bar" }
+    return {"foo": "bar"}
+
 
 def test_lambda_handler(lambda_event):
     ret = app.lambda_handler(lambda_event, "")
     assert ret == {'hello': 'world'}
-{% endif %}
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+key = 'hello'
+value = 'test'
+
+
+def test_get_message(lambda_event, key, value):
+    ret = app.lambda_handler(lambda_event, "")
+    # assert ret == {'hello': 'world'}
+    assert ret == {key: value}
+
+
+def test_get_message(apigw_event, key, value):
+    ret = app.lambda_handler(apigw_event, "")
+    assert ret['statusCode'] == 200
+    assert ret['body'] == json.dumps({key: value})
+
+
+{ % else %}
+
+@pytest.fixture()
+def lambda_event():
+    """ Generates Lambda Event"""
+
+    return {"foo": "bar"}
+
+
+def test_get_message(lambda_event, key, value):
+    ret = app.lambda_handler(lambda_event, "")
+    assert ret == {key: value}
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+def test_runs_on_aws_lambda():
+    ret = app.lambda_handler(lambda_event, "")
+    assert ret == {'hello': 'world'}
+
+
+def test_runs_on_aws_lambda(apigw_event):
+    ret = app.lambda_handler(apigw_event, "")
+    assert ret['statusCode'] == 200
+    assert ret['body'] == json.dumps({'hello': 'world'})
+
+
+{ % else %}
+
+@pytest.fixture()
+def lambda_event():
+    """ Generates Lambda Event"""
+
+    return {"foo": "bar"}
+
+
+def test_lambda_handler(lambda_event):
+    ret = 'AWS_SAM_LOCAL' not in os.environ and 'LAMBDA_TASK_ROOT' in os.environ
+    assert ret == True
